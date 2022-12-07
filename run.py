f695218265e11f7491b2aaca87da3dc3a33e38e0
ancaerena <anca.erena@yahoo.ro>
2022-12-07 19:56:41 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+print("Welcome to the best Battleship game")
+print("Be ready to guess your opponent strategy in placing their ships on the board")
+print("The board has 4 ships with a 5 in size")
+print("For your guesses, please chose numbers between 0 and 4, for both columns and rows")
+
+
