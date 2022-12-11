76063d3f00d544476679d90c7385ae482045c9ee
ancaerena <anca.erena@yahoo.ro>
2022-12-11 13:21:47 +0000

    Add welcome message and get player name functions in main function

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
 from random import randint
 
-print("Welcome to the best Battleship game")
-print("Be ready to guess your opponent strategy in placing their ships on the board")
-print("The board has 4 ships with a 5 in size")
-print("For your guesses, please chose numbers between 0 and 4, for both columns and rows")
+def welcome_message():
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("Welcome to the best Battleship game")
+    print("Be ready to guess your opponent strategy in placing their ships on the board")
+    print("The board size is 5, with 4 ships")
+    print("For your guesses, please chose numbers between 0 and 4, for both columns and rows")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+def get_player_name():
+    """
+    Get player's name to be used further in the game
+    """
+    player = input("Please enter your name:\n")
 
 
+
+def main():
+    welcome_message()
+    get_player_name()
+
+main()
